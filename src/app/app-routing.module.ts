56454a3de21074990d2fda6e66754fb16ba7448c
Sunit121 <sunit.vadhia@gmail.com>
2020-12-24 16:30:09 +0530

    t&c page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DhandaLandingComponent } from './dhanda-landing/dhanda-landing.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
 
 const routes: Routes = [{ path:'',component:DhandaLandingComponent},
+{path:'privacy-policy', component:PrivacyPolicyComponent},
+{path:'t&c',component:TermsAndConditionsComponent},
 { path:'**',redirectTo:''}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { AppComponent } from './app.component';
 import { DhandaLandingComponent } from './dhanda-landing/dhanda-landing.component';
 // import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DhandaLandingComponent
+    DhandaLandingComponent,
+    PrivacyPolicyComponent,
+    TermsAndConditionsComponent
   ],
   imports: [
     BrowserModule,
